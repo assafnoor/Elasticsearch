3ee6fd20ac519ac6a4442a55d7612460e107cef5
Noor Assaf <noorassaf413@gmail.com>
2023-09-22 22:50:03 +0300

    Add project files.

---
+++ Elasticsearch/Program.cs
@@ -0,0 +1,60 @@
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+configurelogs();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+#region helper
+void configurelogs()
+{
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); 
+
+    var configuration=new ConfigurationBuilder().AddJsonFile("appsettings.json",optional:false,reloadOnChange:true).Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(configureELS(configuration,env))
+        .CreateLogger();
+}
+ElasticsearchSinkOptions configureELS(IConfiguration configuration ,string env)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ELKConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat=$"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyyy-MM}"
+        
+    }; 
+}
+#endregion
